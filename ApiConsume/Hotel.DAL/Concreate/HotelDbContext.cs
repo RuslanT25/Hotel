4e8d8e609db2bedf1dcea749da49e07636e0defd
Ruslan <tagizaderuslan25@gmail.com>
2024-01-01 12:15:27 +0400

    Add some folders.

---
+++ ApiConsume/Hotel.DAL/Concreate/HotelDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.DAL.Concreate
+{
+    public class HotelDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-JVM62VU\\SQLEXPRESS;database=Udemy-Hotel;Integrated Security=true");
+        }
+
+    }
+}
