bc5014ca668c36d954780a64641824c47be739b1
Ruslan <tagizaderuslan25@gmail.com>
2024-01-04 11:39:15 +0400

    Update Hotel.Map layer location

---
+++ ApiConsume/Hotel.Map/Hotel.Map/Options/BaseMap.cs
@@ -0,0 +1,22 @@
+﻿using Hotel.Entity.Entities;
+using Hotel.Entity.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Map.Options
+{
+    public abstract class BaseMap<T> : IEntityTypeConfiguration<T> where T : BaseEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.Property(x => x.CreatedDate).HasColumnName("Created time");
+            builder.Property(x => x.ModifiedDate).HasColumnName("Updated time");
+            builder.Property(x => x.DeletedDate).HasColumnName("Deleted time");
+        }
+    }
+}
---
+++ ApiConsume/Hotel.Map/Hotel.Map/Options/TestimonialService.cs
@@ -0,0 +1,25 @@
+﻿using Hotel.Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Map.Options
+{
+    public class TestimonialService : BaseMap<Testimonial>
+    {
+        public override void Configure(EntityTypeBuilder<Testimonial> builder)
+        {
+            base.Configure(builder);
+            builder.Property(x => x.Name)
+                .HasColumnName("Commenter name")
+                .HasMaxLength(50)
+                .IsRequired();
+            builder.Property(x => x.Image)
+                .HasColumnName("Commenter image");
+        }
+    }
+}
